391dd78d0735213280fdf1e80c89d8f369004150
Damian Bernat <48223251+damdam6680@users.noreply.github.com>
2023-06-22 22:31:44 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-%systemDrive%\xampp\mysql\bin\mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ai1_lab12;"
+%systemDrive%\xampp\mysql\bin\mysql -uroot -e "CREATE DATABASE IF NOT EXISTS appklinika;"
 
 if %errorlevel% neq 0 msg %username% "Nie udalo sie utworzyc bazy danych." && exit /b %errorlevel%
 
